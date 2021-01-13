b0162a129e1c4a10d0695e9b52f3ea98084f2fe4
Christopher McCracken <46425436+Chrithtoph@users.noreply.github.com>
2021-01-13 16:28:32 -0700

    Fix pathing

--- Index.js
+++ Index.js
@@ -1,12 +1,11 @@
-var express = require('express');
+var express = require('express'); // import express
 
-var api = require('./StockAPI');
+var api = require('./API/StockAPI'); // import StockAPI
 
 var app = express();
 
 app.use('/', api);
 app.use(express.static('Frontend'));
-
 
 app.listen(3000, function() {
     console.log('Node app is running on port 3000');
